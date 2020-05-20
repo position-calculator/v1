7b4ecd8c2a7fa6ea683988a0e832dc0c8189724c
position-calculator <aliosha789123@gmail.com>
2020-05-20 21:01:47 +0300

    Updates

---
+++ precache-manifest.e07d5898e6e353c2f83ff9b773451fb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3c7c8a1871f17c96740b02e927697d",
+    "url": "/v1/index.html"
+  },
+  {
+    "revision": "7772de58c28232904143",
+    "url": "/v1/static/js/2.4ddd982e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/v1/static/js/2.4ddd982e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3704512dbbea77763d37",
+    "url": "/v1/static/js/main.3efc49ae.chunk.js"
+  },
+  {
+    "revision": "b44c1877697c9b3ca4b0",
+    "url": "/v1/static/js/runtime-main.1171c0ca.js"
+  }
+]);
